454ee59a313e9d3695711280bc7e140ee3c95a3c
Amandeep Singh <a.d.scr71234@gmail.com>
2024-01-05 14:26:03 +0530

    Changed strictQuery to true mongoose

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const cookieParser = require("cookie-parser");
 const authMiddleware = require("./middleware/authMiddleware");
 require("dotenv").config();
 
+mongoose.set("strictQuery", true);
+
 console.log(process.env.DB_URI);
 
 const app = express();
